1c7d67dd2fe6abf2c47ce41d2137a3e203ce87e2
Xavier Pashley <xavipashley@gmail.com>
2023-11-24 10:55:03 +0000

    updated html and javascript

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-class Animal extends Cyberpet {
-    constructor() {
-        super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Wookie specific attribute
-        this.chuckleLevel = 150;
-    }
+// class Animal extends Cyberpet {
+//     constructor() {
+//         super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Wookie specific attribute
+//         this.chuckleLevel = 150;
+//     }
 
-    // Custom method for the Wookie cyberpet
-    food() {
-        if (this.hungerLevel < 300) {
-            this.hungerLevel += 30;
-            this.thirst += 20;
-            this.updateUI();
-        }
-    }
+//     // Custom method for the Wookie cyberpet
+//     food() {
+//         if (this.hungerLevel < 300) {
+//             this.hungerLevel += 30;
+//             this.thirst += 20;
+//             this.updateUI();
+//         }
+//     }
 
-    // Wookie specific overrides of basic attributes and additional ones
-    decreaseStats() {
-        this.hunger += 30;
-        this.thirst += 30;
-        this.happiness -= 40;
-        this.chuckleLevel -= 26;
-    }
+//     // Wookie specific overrides of basic attributes and additional ones
+//     decreaseStats() {
+//         this.hunger += 30;
+//         this.thirst += 30;
+//         this.happiness -= 40;
+//         this.chuckleLevel -= 26;
+//     }
 
-    // Override the updateUI method to display the chuckle level
-    updateUI() {
-        super.updateUI(); // Call the basic class updateUI method, and add the following Wookie specific update
-        document.querySelector(".chuckleLevel").style.width = `${this.chuckleLevel}px`
-    }
-}
+//     // Override the updateUI method to display the chuckle level
+//     updateUI() {
+//         super.updateUI(); // Call the basic class updateUI method, and add the following Wookie specific update
+//         document.querySelector(".chuckleLevel").style.width = `${this.chuckleLevel}px`
+//     }
+// }
 
 // -------------------------------------------------------------------------------------------------------------------------
+let dog = document.getElementById("")
+const startGame = (selectedPet) => {
 
+}
 
-
-Expand (53 lines) CollapseCyberPetJavaScriptlet image = document.getElementById("image");
+// Expand (53 lines) CollapseCyberPetJavaScriptlet image = document.getElementById("image");
 let petMessage = document.getElementById("petMessage");
+
 class Animal {
     constructor(name){
         this.name = name;
@@ -51,6 +54,31 @@ class Animal {
         this.hunger += 10;
     };
 };
+
+tigerChar.addEventListener('click', () => {
+    total = 0;
+    score.textContent = total;
+    info.textContent = "";
+    image.src="";
+    playAgain.style.display="none";
+    roll.style.display = "block";
+})
+dogChar.addEventListener('click', () => {
+    total = 0;
+    score.textContent = total;
+    info.textContent = "";
+    image.src="";
+    playAgain.style.display="none";
+    roll.style.display = "block";
+})
+catChar.addEventListener('click', () => {
+    total = 0;
+    score.textContent = total;
+    info.textContent = "";
+    image.src="";
+    playAgain.style.display="none";
+    roll.style.display = "block";
+})
 
 class Tiger extends Animal {
     constructor(name){
